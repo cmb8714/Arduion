3018aecedfa05580736b41547190f1203c222221
cmb8714 <bennettcole418@gmail.com>
2016-11-02 14:27:30 -0500

    Add files via upload

---
+++ Build_9.ino
@@ -0,0 +1,34 @@
+
+//PhotoResistor Pin
+int lightPin = 0; 
+//LED Pin
+int ledPin = 9;   
+
+#include <Servo.h>
+
+Servo myservo;  
+
+int potpin = 0;  
+int val;    
+
+                  
+void setup()
+{
+  pinMode(ledPin, OUTPUT); 
+  myservo.attach(9);
+}
+ 
+void loop()
+{
+ int lightLevel = analogRead(lightPin); 
+ lightLevel = map(lightLevel, 0, 900, 0, 255);
+ lightLevel = constrain(lightLevel, 0, 255);
+                                           
+ analogWrite(ledPin, lightLevel);  
+
+
+  val = analogRead(potpin);           
+  val = map(val, 0, 1023, 0, 179);    
+  myservo.write(val);                  
+  delay(15);   
+}
